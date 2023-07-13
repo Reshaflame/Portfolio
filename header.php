6e4419d820160615f5b2a62df3429bb988badd9e
Reshaflame <102427924+Reshaflame@users.noreply.github.com>
2023-07-13 22:52:35 +0300

    Update header.php

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
         <nav>
             <ul>
                 <li><a href="index.php">Home</a></li>
-                <li><a href="about.php">CV</a></li>
+                <li><a href="about.php">About Me</a></li>
                 <li><a href="projects.php">Projects</a></li>
                 <li><a href="contact.php">Contact</a></li>
             </ul>
